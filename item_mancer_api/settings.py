3a2a833b1ca1230042e90d30d88fcab0ffb0870d
JanikCodes <piefke2000@gmx.de>
2024-07-15 21:23:16 +0200

    chore: added ip to settings

--- item_mancer_api/settings.py
+++ item_mancer_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lwwvmxt-f$=vpkfdulg))b$&qap28mddqx5$w_#c09z&hhcdvw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.10.3']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
